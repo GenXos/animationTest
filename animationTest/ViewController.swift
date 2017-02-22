4ea9bad703319b5147e230ade2f4dda6d92b6a10
Todd Fields <toddwfields@gmail.com>
2017-02-22 17:46:16 -0600

    animated button menu project

--- animationTest/ViewController.swift
+++ animationTest/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var hockeyButton: UIButton!
+    @IBOutlet weak var footballButton: UIButton!
+    @IBOutlet weak var basketballButton: UIButton!
+    @IBOutlet weak var trophyButton: UIButton!
+    
+    var hCenter: CGPoint!
+    var fCenter: CGPoint!
+    var bCenter: CGPoint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        hCenter = hockeyButton.center
+        fCenter = footballButton.center
+        bCenter = basketballButton.center
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    override func viewDidLayoutSubviews() {
+
+        hockeyButton.center = trophyButton.center
+        footballButton.center = trophyButton.center
+        basketballButton.center = trophyButton.center
+        hockeyButton.alpha = 0
+        footballButton.alpha = 0
+        basketballButton.alpha = 0
+    }
+    
+
+    @IBAction func trophyButtonPressed(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.hockeyButton.center = self.hCenter
+            self.hockeyButton.alpha = 1
+            self.footballButton.center = self.fCenter
+            self.footballButton.alpha = 1
+            self.basketballButton.center = self.bCenter
+            self.basketballButton.alpha = 1
+        }
+    }
 
 }
 
