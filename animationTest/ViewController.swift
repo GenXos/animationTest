093aaa453490d2f96ca279af9e20a4df3a948da0
Todd Fields <toddwfields@gmail.com>
2017-02-22 13:17:50 -0600

    Initial Commit

---
+++ animationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animationTest
+//
+//  Created by Todd Fields on 2017-02-22.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
